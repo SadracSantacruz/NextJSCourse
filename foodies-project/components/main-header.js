fc159a776ce20234cfa0808a89b82cb0987ff06e
Sadrac <sadrac.santacruz@gmail.com>
2023-12-17 21:02:00 -0800

    added and cleaned a lot of code, I also added and broke down a bunch and combouluded code on the website

--- foodies-project/components/main-header.js
+++ foodies-project/components/main-header.js
@@ -8,27 +8,32 @@ import logoImg from "@/assets/logo.png";
 // Style File
 import style from "./main-header.module.css";
 
+import MainHeaderBackground from "./main-header-background";
+
 // This header will be the deafault header that we will be inputing into our layout for the rest of the website
 export default function MainHeader() {
   return (
-    // Create a header element that contains the navbar and other stuff
-    <header className={style.header}>
-      <Link className={style.logo} href="/">
-        <Image src={logoImg} alt="A plate with food in it." priority></Image>
-        NextLevel Food
-      </Link>
+    <>
+      <MainHeaderBackground />
+      {/* Create a header element that contains the navbar and other stuff */}
+      <header className={style.header}>
+        <Link className={style.logo} href="/">
+          <Image src={logoImg} alt="A plate with food in it." priority></Image>
+          NextLevel Food
+        </Link>
 
-      {/* This is the Navbar, that contains some links that will allow mobility in the website*/}
-      <nav className={style.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Browse Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        {/* This is the Navbar, that contains some links that will allow mobility in the website*/}
+        <nav className={style.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
