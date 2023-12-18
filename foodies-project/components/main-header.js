aebcc2716c3a0e12429dbda6e70fe6435aa201c1
Sadrac <sadrac.santacruz@gmail.com>
2023-12-17 20:51:50 -0800

    added some comments

--- foodies-project/components/main-header.js
+++ foodies-project/components/main-header.js
@@ -1,16 +1,24 @@
+// NextJS packages
 import Link from "next/link";
-import logoImg from "@/assets/logo.png";
-import style from "./main-header.module.css";
 import Image from "next/image";
 
+// Images
+import logoImg from "@/assets/logo.png";
+
+// Style File
+import style from "./main-header.module.css";
+
+// This header will be the deafault header that we will be inputing into our layout for the rest of the website
 export default function MainHeader() {
   return (
+    // Create a header element that contains the navbar and other stuff
     <header className={style.header}>
       <Link className={style.logo} href="/">
         <Image src={logoImg} alt="A plate with food in it." priority></Image>
         NextLevel Food
       </Link>
 
+      {/* This is the Navbar, that contains some links that will allow mobility in the website*/}
       <nav className={style.nav}>
         <ul>
           <li>
