34572fe1bc843f1be02924c4489891e6df666c72
Sadrac <sadrac.santacruz@gmail.com>
2023-12-22 21:27:28 -0800

    fixed a weird error

--- foodies-project/app/meals/page.js
+++ foodies-project/app/meals/page.js
@@ -1,15 +1,26 @@
-import style from "./page.module.css";
+import Link from "next/link";
 
-export default function MealPage() {
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
+export default function MealsPage() {
   return (
     <>
-      <header className={style.header}>
+      <header className={classes.header}>
         <h1>
-          Delicious Meals, created{" "}
-          <span className={style.highlight}>by you</span>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
         </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
       </header>
-      <main className={style.main}></main>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
     </>
   );
 }
