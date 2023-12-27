5330d24be81257d2fc281bf641e1f84031ea2a2a
Sadrac <sadrac.santacruz@gmail.com>
2023-12-27 14:26:59 -0800

    check

--- foodies-project/lib/meals.js
+++ foodies-project/lib/meals.js
@@ -2,8 +2,21 @@ import sql from "better-sqlite3";
 
 const db = sql("meals.db");
 
+/**
+ * We use the async becuase we are usin gthe the await method, we don't use it for the method below
+ * because we are not using any dealay
+ * @returns the data from the meal on from the databae
+ */
 export async function getMeals() {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   //throw new Error("Loading Meals Failed");
   return db.prepare("SELECT * FROM meals").all();
+}
+
+export function getMeal(slug) {
+  /**
+   * We could do "" + slug, none the less this open for SQL injections which we don't want that's why we use
+   * .get(slug)
+   */
+  return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
 }
