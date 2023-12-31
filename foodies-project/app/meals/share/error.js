b9362e09dbedfbd07e53ed3521f6cadb45052405
Sadrac <sadrac.santacruz@gmail.com>
2023-12-30 16:49:22 -0800

    added an error page for the share page, and also making sure that the user doesn't not remove the requiered comoponed so their ruin the submission to the db

---
+++ foodies-project/app/meals/share/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error({ error }) {
+  return (
+    <main className="error">
+      <h1>An error Ocurred!</h1>
+      <p>Failed to crate a meal</p>
+    </main>
+  );
+}
