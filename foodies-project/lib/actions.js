b7e1db866d93fe042f3a175f89804cfdab5fa137
Sadrac <sadrac.santacruz@gmail.com>
2024-01-02 19:51:35 -0800

    fixed the error from before, none the less the images are not working since i need to store tthe images in a sever like AWS instead of local saved images

--- foodies-project/lib/actions.js
+++ foodies-project/lib/actions.js
@@ -3,12 +3,13 @@
 import { redirect } from "next/navigation";
 
 import { saveMeal } from "./meals";
+import { revalidatePath } from "next/cache";
 
 function isInvalidText(text) {
   return !text || text.trim() === "";
 }
 
-export async function shareMeal(formData) {
+export async function shareMeal(prevState, formData) {
   const meal = {
     title: formData.get("title"),
     summary: formData.get("summary"),
@@ -26,10 +27,14 @@ export async function shareMeal(formData) {
     isInvalidText(meal.creator_email) ||
     !meal.creator_email.includes("@") ||
     !meal.image ||
-    meal.image.size() === 0
+    meal.image.size === 0
   ) {
-    throw new Error("Invalid Input");
+    return {
+      message: "Invalid input.",
+    };
   }
+
   await saveMeal(meal);
+  revalidatePath("/meals");
   redirect("/meals");
 }
